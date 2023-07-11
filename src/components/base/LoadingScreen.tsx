572a3997e2b93d04fabf6d9abdba49af585b8329
Maurice Yang <mauriceyangmy28@gmail.com>
2023-07-11 15:41:02 +0700

    feat: Added content for splash screen and loading screen

--- src/components/base/LoadingScreen.tsx
+++ src/components/base/LoadingScreen.tsx
@@ -5,7 +5,7 @@ const LoadingScreen = () => {
     return (
         <main className='z-50 flex flex-col items-center justify-center w-full h-screen gap-3 bg-slate-50'>
             <Image src="/icon.png" alt='Logo' width={128} height={128} />
-            <p className='text-2xl font-semibold text-center'>
+            <p className='text-lg font-semibold text-center'>
                 Sedang memuat...
             </p>
         </main>
